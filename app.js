947eeccefc7906f5bd84c683f49e1a0248a49afc
Anahita02 <annataghadosi@gmail.com>
2023-10-19 18:42:06 +0330

    API

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast(){
+function displayForecast(response){
  let forecastElement=document.querySelector("#forecast");
  let days=["Sun","Mon","Tue","Wed","Thu","Fri"];
  let forecastHTML=`<div class="row">`;
@@ -43,6 +43,12 @@ function displayForecast(){
  })
   forecastHTML=forecastHTML+`</div>`                      
  forecastElement.innerHTML=forecastHTML;
+}
+
+function getForecast(coordinates){
+ let apiKey = "cabdbda40038ba7d1165b953b1c7bd6c";
+ let apiUrl=`https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey} units=metric`;
+ axios.get(apiUrl).then(displayForecast);
 }
 
 function displayTemperature(response) {
@@ -67,6 +73,9 @@ function displayTemperature(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+ 
+ getForecast(response.data.coord);
+
 }
 
 function search(city) {
